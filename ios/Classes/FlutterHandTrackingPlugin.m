17526a7c160975d102ce967b85e97c96701e57d2
Zaihang <zaihang822@gmail.com>
2020-02-06 16:31:31 +0800

    Inital Project

---
+++ ios/Classes/FlutterHandTrackingPlugin.m
@@ -0,0 +1,20 @@
+#import "FlutterHandTrackingPlugin.h"
+
+@implementation FlutterHandTrackingPlugin
++ (void)registerWithRegistrar:(NSObject<FlutterPluginRegistrar>*)registrar {
+  FlutterMethodChannel* channel = [FlutterMethodChannel
+      methodChannelWithName:@"flutter_hand_tracking_plugin"
+            binaryMessenger:[registrar messenger]];
+  FlutterHandTrackingPlugin* instance = [[FlutterHandTrackingPlugin alloc] init];
+  [registrar addMethodCallDelegate:instance channel:channel];
+}
+
+- (void)handleMethodCall:(FlutterMethodCall*)call result:(FlutterResult)result {
+  if ([@"getPlatformVersion" isEqualToString:call.method]) {
+    result([@"iOS " stringByAppendingString:[[UIDevice currentDevice] systemVersion]]);
+  } else {
+    result(FlutterMethodNotImplemented);
+  }
+}
+
+@end
