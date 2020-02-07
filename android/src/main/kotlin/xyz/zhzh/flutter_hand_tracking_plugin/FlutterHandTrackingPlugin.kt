296790a3531dc0ecabcbc18e66eaaa7be6a2d374
Zaihang <zaihang822@gmail.com>
2020-02-07 16:19:40 +0800

    add mediapipe lib

--- android/src/main/kotlin/xyz/zhzh/flutter_hand_tracking_plugin/FlutterHandTrackingPlugin.kt
+++ android/src/main/kotlin/xyz/zhzh/flutter_hand_tracking_plugin/FlutterHandTrackingPlugin.kt
@@ -6,6 +6,11 @@ import android.graphics.SurfaceTexture
 import android.view.Surface
 import android.view.SurfaceView
 import androidx.annotation.NonNull
+import com.google.mediapipe.components.CameraHelper
+import com.google.mediapipe.components.ExternalTextureConverter
+import com.google.mediapipe.components.FrameProcessor
+import com.google.mediapipe.formats.proto.LandmarkProto
+import com.google.mediapipe.glutil.EglManager
 import io.flutter.plugin.common.MethodCall
 import io.flutter.plugin.common.MethodChannel
 import io.flutter.plugin.common.MethodChannel.MethodCallHandler
@@ -29,21 +34,53 @@ class FlutterHandTrackingPlugin(texturesRegistry: TextureRegistry) : MethodCallH
     // {@link SurfaceView} that displays the camera-preview frames processed by a MediaPipe graph.
     private var previewDisplayView: SurfaceView? = null
     // Creates and manages an {@link EGLContext}.
-//    private var eglManager: EglManager? = null
+    private var eglManager: EglManager? = null
     // Sends camera-preview frames into a MediaPipe graph for processing, and displays the processed
     // frames onto a {@link Surface}.
-//    private var processor: FrameProcessor? = null
+    private var processor: FrameProcessor? = null
     // Converts the GL_TEXTURE_EXTERNAL_OES texture from Android camera into a regular texture to be
     // consumed by {@link FrameProcessor} and the underlying MediaPipe graph.
-//    private var converter: ExternalTextureConverter? = null
+    private var converter: ExternalTextureConverter? = null
 
     private val entry: TextureRegistry.SurfaceTextureEntry = texturesRegistry.createSurfaceTexture()
 
     private var surface: Surface? = null
 
+    init { // Load all native libraries needed by the app.
+        System.loadLibrary("mediapipe_jni")
+        System.loadLibrary("opencv_java3")
+    }
 
     companion object {
         private const val NAMESPACE: String = "xyz.zhzh.flutter_hand_tracking_plugin"
+        private const val BINARY_GRAPH_NAME = "handtrackinggpu.binarypb"
+        private const val INPUT_VIDEO_STREAM_NAME = "input_video"
+        private const val OUTPUT_VIDEO_STREAM_NAME = "output_video"
+        private const val OUTPUT_HAND_PRESENCE_STREAM_NAME = "hand_presence"
+        private const val OUTPUT_LANDMARKS_STREAM_NAME = "hand_landmarks"
+        private val CAMERA_FACING = CameraHelper.CameraFacing.FRONT
+        // Flips the camera-preview frames vertically before sending them into FrameProcessor to be
+        // processed in a MediaPipe graph, and flips the processed frames back when they are displayed.
+        // This is needed because OpenGL represents images assuming the image origin is at the bottom-left
+        // corner, whereas MediaPipe in general assumes the image origin is at top-left.
+        private const val FLIP_FRAMES_VERTICALLY = true
+
+        private fun getLandmarksDebugString(landmarks: LandmarkProto.NormalizedLandmarkList): String {
+            var landmarksString = ""
+            for ((landmarkIndex, landmark) in landmarks.landmarkList.withIndex()) {
+                landmarksString += ("\t\tLandmark["
+                        + landmarkIndex
+                        + "]: ("
+                        + landmark.x
+                        + ", "
+                        + landmark.y
+                        + ", "
+                        + landmark.z
+                        + ")\n")
+            }
+            return landmarksString
+        }
+
         @JvmStatic
         fun registerWith(registrar: Registrar) {
             val channel = MethodChannel(registrar.messenger(), NAMESPACE)
@@ -62,6 +99,8 @@ class FlutterHandTrackingPlugin(texturesRegistry: TextureRegistry) : MethodCallH
                 previewFrameTexture = entry.surfaceTexture()
                 previewFrameTexture!!.setDefaultBufferSize(width, height)
 
+//                val surfaceView:SurfaceView = SurfaceView()
+
                 surface = Surface(previewFrameTexture)
 
                 val canvas: Canvas = surface!!.lockCanvas(null)
@@ -75,8 +114,9 @@ class FlutterHandTrackingPlugin(texturesRegistry: TextureRegistry) : MethodCallH
                 result.success(entry.id())
             }
             "dispose" -> {
-                var textureId: Long = call.arguments as Long
+                val textureId: Long = call.arguments as Long
                 // TODO: Render dispose by textureId
+                result.success(textureId)
             }
             else -> result.notImplemented()
         }
