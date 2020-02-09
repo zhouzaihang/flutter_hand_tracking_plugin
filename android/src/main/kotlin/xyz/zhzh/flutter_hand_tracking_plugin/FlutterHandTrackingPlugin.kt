993d9c9a66f7a77fc760bd1aa62ecc2887be0791
Zaihang <zaihang822@gmail.com>
2020-02-09 17:32:33 +0800

    access camera and using a MediaPipe graph

--- android/src/main/kotlin/xyz/zhzh/flutter_hand_tracking_plugin/FlutterHandTrackingPlugin.kt
+++ android/src/main/kotlin/xyz/zhzh/flutter_hand_tracking_plugin/FlutterHandTrackingPlugin.kt
@@ -2,8 +2,19 @@ package xyz.zhzh.flutter_hand_tracking_plugin
 
 import android.app.Activity
 import android.graphics.SurfaceTexture
+import android.util.Log
+import android.util.Size
+import android.view.SurfaceHolder
 import android.view.SurfaceView
+import android.view.View
 import androidx.annotation.NonNull
+import com.google.mediapipe.components.*
+import com.google.mediapipe.formats.proto.LandmarkProto
+import com.google.mediapipe.framework.AndroidAssetUtil
+import com.google.mediapipe.framework.Packet
+import com.google.mediapipe.framework.PacketGetter
+import com.google.mediapipe.glutil.EglManager
+import com.google.protobuf.InvalidProtocolBufferException
 import io.flutter.plugin.common.MethodCall
 import io.flutter.plugin.common.MethodChannel
 import io.flutter.plugin.common.MethodChannel.MethodCallHandler
@@ -14,7 +25,36 @@ import io.flutter.plugin.platform.PlatformView
 /** FlutterHandTrackingPlugin */
 class FlutterHandTrackingPlugin(r: Registrar, id: Int) : PlatformView, MethodCallHandler {
     companion object {
+        private const val TAG = "HandTrackingPlugin"
         private const val NAMESPACE = "plugins.zhzh.xyz/flutter_hand_tracking_plugin"
+        private const val BINARY_GRAPH_NAME = "handtrackinggpu.binarypb"
+        private const val INPUT_VIDEO_STREAM_NAME = "input_video"
+        private const val OUTPUT_VIDEO_STREAM_NAME = "output_video"
+        private const val OUTPUT_HAND_PRESENCE_STREAM_NAME = "hand_presence"
+        private const val OUTPUT_LANDMARKS_STREAM_NAME = "hand_landmarks"
+        private val CAMERA_FACING = CameraHelper.CameraFacing.FRONT
+        // Flips the camera-preview frames vertically before sending them into FrameProcessor to be
+        // processed in a MediaPipe graph, and flips the processed frames back when they are displayed.
+        // This is needed because OpenGL represents images assuming the image origin is at the bottom-left
+        // corner, whereas MediaPipe in general assumes the image origin is at top-left.
+        private const val FLIP_FRAMES_VERTICALLY = true
+
+        private fun getLandmarksDebugString(landmarks: LandmarkProto.NormalizedLandmarkList): String {
+            var landmarksString = ""
+            for ((landmarkIndex, landmark) in landmarks.landmarkList.withIndex()) {
+                landmarksString += ("\t\tLandmark["
+                        + landmarkIndex
+                        + "]: ("
+                        + landmark.x
+                        + ", "
+                        + landmark.y
+                        + ", "
+                        + landmark.z
+                        + ")\n")
+            }
+            return landmarksString
+        }
+
         @JvmStatic
         fun registerWith(registrar: Registrar) {
             registrar.platformViewRegistry().registerViewFactory(
@@ -34,9 +74,67 @@ class FlutterHandTrackingPlugin(r: Registrar, id: Int) : PlatformView, MethodCal
     private var previewFrameTexture: SurfaceTexture? = null
     // {@link SurfaceView} that displays the camera-preview frames processed by a MediaPipe graph.
     private var previewDisplayView: SurfaceView = SurfaceView(r.context())
+    // Creates and manages an {@link EGLContext}.
+    private var eglManager: EglManager? = null
+    // Sends camera-preview frames into a MediaPipe graph for processing, and displays the processed
+    // frames onto a {@link Surface}.
+    private var processor: FrameProcessor? = null
+    // Converts the GL_TEXTURE_EXTERNAL_OES texture from Android camera into a regular texture to be
+    // consumed by {@link FrameProcessor} and the underlying MediaPipe graph.
+    private var converter: ExternalTextureConverter? = null
+    // Handles camera access via the {@link CameraX} Jetpack support library.
+    private var cameraHelper: CameraXPreviewHelper? = null
 
     init {
         this.methodChannel.setMethodCallHandler(this)
+        setupPreviewDisplayView()
+        // Initialize asset manager so that MediaPipe native libraries can access the app assets, e.g.,
+        // binary graphs.
+        AndroidAssetUtil.initializeNativeAssetManager(activity)
+        eglManager = EglManager(null)
+        processor = FrameProcessor(
+                activity,
+                eglManager!!.nativeContext,
+                BINARY_GRAPH_NAME,
+                INPUT_VIDEO_STREAM_NAME,
+                OUTPUT_VIDEO_STREAM_NAME)
+        processor!!.videoSurfaceOutput.setFlipY(FLIP_FRAMES_VERTICALLY)
+        processor!!.addPacketCallback(
+                OUTPUT_HAND_PRESENCE_STREAM_NAME
+        ) { packet: Packet ->
+            val handPresence = PacketGetter.getBool(packet)
+            if (!handPresence) {
+                Log.d(
+                        TAG,
+                        "[TS:" + packet.timestamp + "] Hand presence is false, no hands detected.")
+            }
+        }
+        processor!!.addPacketCallback(
+                OUTPUT_LANDMARKS_STREAM_NAME
+        ) { packet: Packet ->
+            val landmarksRaw = PacketGetter.getProtoBytes(packet)
+            try {
+                val landmarks = LandmarkProto.NormalizedLandmarkList.parseFrom(landmarksRaw)
+                if (landmarks == null) {
+                    Log.d(TAG, "[TS:" + packet.timestamp + "] No hand landmarks.")
+                    return@addPacketCallback
+                }
+                // Note: If hand_presence is false, these landmarks are useless.
+                Log.d(
+                        TAG,
+                        "[TS:"
+                                + packet.timestamp
+                                + "] #Landmarks for hand: "
+                                + landmarks.landmarkCount)
+                Log.d(TAG, getLandmarksDebugString(landmarks))
+            } catch (e: InvalidProtocolBufferException) {
+                Log.e(TAG, "Couldn't Exception received - $e")
+                return@addPacketCallback
+            }
+        }
+        PermissionHelper.checkAndRequestCameraPermissions(activity)
+
+        if (PermissionHelper.cameraPermissionsGranted(activity)) onResume()
     }
 
     override fun onMethodCall(@NonNull call: MethodCall, @NonNull result: Result) {
@@ -52,6 +150,55 @@ class FlutterHandTrackingPlugin(r: Registrar, id: Int) : PlatformView, MethodCal
     }
 
     override fun dispose() {
-        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+        converter!!.close()
+    }
+
+    private fun onResume() {
+        converter = ExternalTextureConverter(eglManager!!.context)
+        converter!!.setFlipY(FLIP_FRAMES_VERTICALLY)
+        converter!!.setConsumer(processor)
+        if (PermissionHelper.cameraPermissionsGranted(activity)) {
+            startCamera()
+        }
+    }
+
+    private fun setupPreviewDisplayView() {
+        previewDisplayView.visibility = View.GONE
+        previewDisplayView.holder.addCallback(
+                object : SurfaceHolder.Callback {
+                    override fun surfaceCreated(holder: SurfaceHolder) {
+                        processor!!.videoSurfaceOutput.setSurface(holder.surface)
+                    }
+
+                    override fun surfaceChanged(holder: SurfaceHolder, format: Int, width: Int, height: Int) { // (Re-)Compute the ideal size of the camera-preview display (the area that the
+                        // camera-preview frames get rendered onto, potentially with scaling and rotation)
+                        // based on the size of the SurfaceView that contains the display.
+                        val viewSize = Size(width, height)
+                        val displaySize = cameraHelper!!.computeDisplaySizeFromViewSize(viewSize)
+                        val isCameraRotated = cameraHelper!!.isCameraRotated
+                        // Connect the converter to the camera-preview frames as its input (via
+                        // previewFrameTexture), and configure the output width and height as the computed
+                        // display size.
+                        converter!!.setSurfaceTextureAndAttachToGLContext(
+                                previewFrameTexture,
+                                if (isCameraRotated) displaySize.height else displaySize.width,
+                                if (isCameraRotated) displaySize.width else displaySize.height)
+                    }
+
+                    override fun surfaceDestroyed(holder: SurfaceHolder) {
+                        processor!!.videoSurfaceOutput.setSurface(null)
+                    }
+                })
+    }
+
+    private fun startCamera() {
+        cameraHelper = CameraXPreviewHelper()
+        cameraHelper!!.setOnCameraStartedListener { surfaceTexture: SurfaceTexture? ->
+            previewFrameTexture = surfaceTexture
+            // Make the display view visible to start showing the preview. This triggers the
+            // SurfaceHolder.Callback added to (the holder of) previewDisplayView.
+            previewDisplayView.visibility = View.VISIBLE
+        }
+        cameraHelper!!.startCamera(activity, CAMERA_FACING,  /*surfaceTexture=*/null)
     }
 }
